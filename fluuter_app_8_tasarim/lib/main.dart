c1608069cd623cae827ab565752eafe134e2982d
Emre AKCİN <akcin18@itu.edu.tr>
2020-08-30 11:45:57 +0300

    inner drawre

--- fluuter_app_8_tasarim/lib/main.dart
+++ fluuter_app_8_tasarim/lib/main.dart
@@ -1,117 +1,182 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_inner_drawer/inner_drawer.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Tasarım',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: "El Yazisi",
+        primarySwatch: Colors.amber,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Drawer(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        centerTitle: true,
+        title: Text(
+          "Flutter Tasarım",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 24),
+        ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: FontBody(),
+    );
+  }
+}
+
+class FontBody extends StatefulWidget {
+  @override
+  _FontBodyState createState() => _FontBodyState();
+}
+
+class _FontBodyState extends State<FontBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.amber.shade400,
+      child: Center(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          mainAxisSize: MainAxisSize.max,
+          children: [
             Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              "Emre  Akcin",
+              style: TextStyle(
+                  fontFamily: 'El Yazisi',
+                  fontSize: 40,
+                  fontWeight: FontWeight.w700),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class Drawer extends StatefulWidget {
+  @override
+  _DrawerState createState() => _DrawerState();
+}
+
+class _DrawerState extends State<Drawer> {
+  double vali = 0;
+  InnerDrawerDirection yon = InnerDrawerDirection.start;
+  Color renk = Colors.amber;
+  final GlobalKey<InnerDrawerState> _innerDrawerKey =
+      GlobalKey<InnerDrawerState>();
+  @override
+  Widget build(BuildContext context) {
+    return InnerDrawer(
+      key: _innerDrawerKey,
+      onTapClose: true, // default false
+      swipe: true, // default true
+      colorTransitionChild: Colors.blue, // default Color.black54
+      colorTransitionScaffold: Colors.black54, // default Color.black54
+
+      //When setting the vertical offset, be sure to use only top or bottom
+      offset: IDOffset.only(bottom: 0.05, right: 0.0, left: 0.4),
+
+      scale: IDOffset.horizontal(1.0), // set the offset in both directions
+
+      proportionalChildArea: true, // default true
+      borderRadius: 30, // default 0
+      leftAnimationType: InnerDrawerAnimation.static, // default static
+      rightAnimationType: InnerDrawerAnimation.quadratic,
+      backgroundDecoration: BoxDecoration(
+          color: Colors.green), // default  Theme.of(context).backgroundColor
+
+      //when a pointer that is in contact with the screen and moves to the right or left
+      onDragUpdate: (double val, InnerDrawerDirection direction) {
+        vali = val;
+        yon = direction;
+
+        if (vali == 1.0 && yon == InnerDrawerDirection.start) {
+          setState(() {
+            renk = Colors.teal;
+          });
+        } else if (vali == 1.0 && yon == InnerDrawerDirection.end) {
+          setState(() {
+            renk = Colors.indigoAccent;
+          });
+        } else {
+          setState(() {
+            renk = Colors.amber;
+          });
+        }
+        // return values between 1 and 0
+        print(vali);
+        print(yon);
+        //print(val);
+        // check if the swipe is to the right or to the left
+        //print(direction == InnerDrawerDirection.start);
+      },
+
+      innerDrawerCallback: (a) =>
+          print(a), // return  true (open) or false (close)
+      leftChild: Container(
+        child: ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              child: ListTile(
+                title: Text("Emre Akcin"),
+                subtitle: Text(index.toString()),
+              ),
+            );
+          },
+          itemCount: 5,
+        ),
+      ), // required if rightChild is not set
+      rightChild: Container(), // requir
+      scaffold: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_forward_ios),
+            onPressed: () {
+              
+              setState(() {
+                _innerDrawerKey.currentState.toggle(direction: InnerDrawerDirection.start);
+                _innerDrawerKey.currentState.open();
+              });
+            },
+          ),
+          title: Text(
+            "Flutter Tasarım",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+        body: Container(
+          color: renk,
+          child: Center(
+            child: Text(
+              "Emre Akcin",
+              style: TextStyle(fontSize: 40),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _toggle() {
+    _innerDrawerKey.currentState.toggle(
+        // direction is optional
+        // if not set, the last direction will be used
+        //InnerDrawerDirection.start OR InnerDrawerDirection.end
+        direction: InnerDrawerDirection.end);
   }
 }
